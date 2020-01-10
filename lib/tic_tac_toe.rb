1e6dc29d130a1744da54a334c5dbb6caa9367cd6
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-10 18:09:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,5 +41,12 @@ WIN_COMBINATIONS [
       index.between?(0,8) && !position_taken?(index)
     end
     
-    
+    def turn_count
+      number_of_turns = 0
+      @board.each do |space|
+      if space == "X" || space == "O"
+        number_of_turns += 1
+      end
+    end
+    return number_of_turns
     
